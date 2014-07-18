5a1febf64a4c784c02196958a37dfbd7d96861c2
Damon A. Leeman <datleeman@icloud.com>
2014-07-18 14:52:46 -0700

    Updated Similarity Matrix
    Updated the SQL that creates the similarity matrix to only
    include the docids in question. Greatly increased the
    speed of the query.
    
    [Assignment: 2 Problem: h]

--- assignment2/test.sql
+++ assignment2/test.sql
@@ -2,5 +2,7 @@ CREATE VIEW SimilarityMatrix
 AS
 SELECT Frequency.docid,TransFreq.term, SUM(Frequency.count * TransFreq.count)
   FROM Frequency, TransFreq
- WHERE Frequency.docid = TransFreq.docid
+ WHERE 
+	Frequency.docid in ('10080_txt_crude' ,'17035_txt_earn') and
+	Frequency.docid = TransFreq.docid
  GROUP BY Frequency.docid,TransFreq.term;
